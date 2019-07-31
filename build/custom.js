2d078fcb9d9abe2052673c88be50621d55fb9557
Jimmy <ciaffardini.g@gmail.com>
2019-07-31 11:16:19 +0100

    jquery

--- build/custom.js
+++ build/custom.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    $("div[data-framer-name='auctionbtn']").click(function() {
+    $("[name='auctionbtn']").click(function() {
         console.log("clicked")
     })
 });
